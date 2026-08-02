e423c4a32cb3002c55af4fdd53979bbeba2330ea
Vangmayi Muppalla <vangmayimuppalla1@gmail.com>
2026-08-02 23:40:47 +0530

    Update monohybrid.py
    added phenotypic ratio

--- monohybrid.py
+++ monohybrid.py
@@ -41,7 +41,7 @@ print(parent1[0] , " | ",offspring1 , "| " ,offspring2)
 print("---+-----+------")
 print(parent1[1] , " | ", offspring3 , "| " ,offspring4)
 
-TT_count = 0
+TT_count = 0        #GENOTYE RATIO
 Tt_count = 0
 tt_count = 0
 
@@ -71,11 +71,35 @@ if offspring4 == "TT":
 if offspring4 == "Tt":
     Tt_count = Tt_count + 1
 if offspring4 == "tt":
-    tt_count = tt_count + 1
-
+    tt_count = tt_count + 1 
 
 print ("GENOTYPE RATIO:")
 print("TT:", TT_count)
 print("Tt:", Tt_count)
 print("tt:", tt_count)
+
+dominant_count = 0                      #PHENOTYPE RATIO
+recessive_count = 0
+
+if offspring1[0].isupper() or offspring1[1].isupper():
+    dominant_count = dominant_count + 1
+else: recessive_count = recessive_count + 1
+
+if offspring2[0].isupper() or offspring2[1].isupper():
+    dominant_count = dominant_count + 1
+else: recessive_count = recessive_count + 1
+
+if offspring3[0].isupper() or offspring3[1].isupper():
+    dominant_count = dominant_count + 1
+else: recessive_count = recessive_count + 1
+
+if offspring4[0].isupper() or offspring4[1].isupper():
+    dominant_count = dominant_count + 1
+else: recessive_count = recessive_count + 1
+
+print ("PHENOTYPE RATIO:")
+print("Dominant:", dominant_count)
+print("Recessive:", recessive_count)
+
+
 
